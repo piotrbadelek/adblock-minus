625839463837c0fc41457633eff641699f970d39
ProgramistaZpolski <piotrekbadelek@gmail.com>
2020-11-16 18:38:33 +0100

    Wersja jako rozrzeszenie

---
+++ adm.js
@@ -0,0 +1,6 @@
+document.querySelector(".panel-ad345").style.backgroundImage = 'none';
+document.querySelector(".panel-ad345").style.display = 'none';
+document.addEventListener("turbolinks:load", function () {
+    document.querySelector(".panel-ad345").style.backgroundImage = 'none';
+    document.querySelector(".panel-ad345").style.display = 'none';
+})
