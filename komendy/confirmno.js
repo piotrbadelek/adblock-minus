ceb06c12795e40b21ecaf5865ca87fc494878a82
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-11-16 09:37:13 +0100

    Delete confirmno.js

--- komendy/confirmno.js
+++
@@ -1,9 +0,0 @@
-const deeta = require("../zabij-sie-db.js");
-module.exports = {
-    name: 'confirmno',
-    description: 'Wyłącz powiadomienia',
-    execute(message, args) {
-        deeta.addToTableZSDB("./serw.txt", message.guild.id + "\n");
-        message.reply('Wyłączono powiadomienia o blokadzie reklamy!');
-    },
-};
