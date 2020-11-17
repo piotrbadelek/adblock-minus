91e9de0470e6e704c85ce0cd6ae62c060e6ba724
liskqu <56258709+lisqu16@users.noreply.github.com>
2020-11-17 10:48:37 +0100

    Update adm.js

--- adm.js
+++ adm.js
@@ -1,6 +1,8 @@
-document.querySelector(".panel-ad345").style.backgroundImage = 'none';
-document.querySelector(".panel-ad345").style.display = 'none';
-document.addEventListener("turbolinks:load", function () {
-    document.querySelector(".panel-ad345").style.backgroundImage = 'none';
-    document.querySelector(".panel-ad345").style.display = 'none';
-})
+let element;
+elements = document.querySelectorAll(".flex.category.hover-pointer")
+for (let i = 0; i < elements.length; i++) {
+    if (!elements[i].onclick) continue;
+    if (elements[i].onclick.toString().includes("window.open")) element=elements[i];
+};
+element.style.backgroundImage = 'none';
+element.style.display = 'none';
