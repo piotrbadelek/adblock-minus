da24382949b55886cbbc1f783a59badc6231ee70
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-11-17 10:53:39 +0100

    Update adm.js

--- adm.js
+++ adm.js
@@ -2,7 +2,17 @@ let element;
 elements = document.querySelectorAll(".flex.category.hover-pointer")
 for (let i = 0; i < elements.length; i++) {
     if (!elements[i].onclick) continue;
-    if (elements[i].onclick.toString().includes("window.open")) element=elements[i];
+    if (elements[i].onclick.toString().includes("window.open")) element = elements[i];
 };
 element.style.backgroundImage = 'none';
 element.style.display = 'none';
+document.addEventListener("turbolinks:load", function () {
+    let element;
+    elements = document.querySelectorAll(".flex.category.hover-pointer")
+    for (let i = 0; i < elements.length; i++) {
+        if (!elements[i].onclick) continue;
+        if (elements[i].onclick.toString().includes("window.open")) element = elements[i];
+    };
+    element.style.backgroundImage = 'none';
+    element.style.display = 'none';
+}) 
